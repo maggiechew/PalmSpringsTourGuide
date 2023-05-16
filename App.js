492656238d679f51956a3a4b4ab301473fe2c12c
Maggie Chew <105380332+maggiechew@users.noreply.github.com>
2023-05-16 00:23:21 -0600

    Updates

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { RootNavigator } from './navigation/RootNavigator';
+import { AuthenticatedUserProvider } from './providers';
+import { AvatarPickProvider } from './providers/AvatarPickProvider';
 import { useFonts } from 'expo-font';
-import LoginScreen from './screens/LoginScreen';
+import { View, ActivityIndicator } from 'react-native';
 
-const Stack = createNativeStackNavigator();
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { useFonts } from 'expo-font';
+// import LoginScreen from './screens/LoginScreen';
+
+// const Stack = createNativeStackNavigator();
 
 export default function App() {
 
@@ -25,22 +33,12 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="LoginScreen"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AvatarPickProvider>
+      <AuthenticatedUserProvider>
+        <SafeAreaProvider>
+          <RootNavigator />
+        </SafeAreaProvider>
+      </AuthenticatedUserProvider>
+    </AvatarPickProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
