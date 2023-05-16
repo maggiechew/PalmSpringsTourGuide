2b864e68b54da138bde5507decc87d38a5c91241
Maggie Chew <105380332+maggiechew@users.noreply.github.com>
2023-05-15 18:01:52 -0600

    Getting Set Up

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    RWLight: require('./assets/fonts/Raleway-Light.ttf'),
+    RWRegular: require('./assets/fonts/Raleway-Regular.ttf'),
+    RWBold: require('./assets/fonts/Raleway-Bold.ttf'),
+    'Limelight-Regular': require('./assets/fonts/Limelight-Regular.ttf')
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <ActivityIndicator size='large' />
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="LoginScreen"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ config/images.js
@@ -0,0 +1,5 @@
+export const Images = {
+    logo: require('../assets/icon.png')
+
+    //TODO: replace this
+  };
