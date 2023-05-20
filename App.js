ff76d9f459cba5846a5ce984adb1e03c3f4c9b70
Maggie Chew <105380332+maggiechew@users.noreply.github.com>
2023-05-20 13:17:48 -0600

    Removed unnecessary imports

--- App.js
+++ App.js
@@ -6,14 +6,6 @@ import { AvatarPickProvider } from './providers/AvatarPickProvider';
 import { useFonts } from 'expo-font';
 import { View, ActivityIndicator } from 'react-native';
 
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import { useFonts } from 'expo-font';
-// import LoginScreen from './screens/LoginScreen';
-
-// const Stack = createNativeStackNavigator();
 
 export default function App() {
 
